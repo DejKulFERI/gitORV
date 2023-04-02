a6c78e610d6994180336b4938fd4c7f72573e112
DejKulFERI <dejan.kulovec@student.um.si>
2023-04-02 21:26:56 +0200

    Tretji commit

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import cv2
+import numpy as np
 
-img = cv2.imread('images/Slika_1.jpg')
-img = cv2.resize(img, (600, 500))
+def my_roberts(slika):
+    x = np.array([[1, 0], [0, -1]])
+    y = np.array([[0, 1], [-1, 0]])
 
-cv2.imshow('Image', img)
+    slika = slika.astype(np.float32)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    edgeX = cv2.filter2D(slika, -1, x)
+    edgeY = cv2.filter2D(slika, -1, y)
+
+    edges_mag = np.sqrt(np.square(edgeX) + np.square(edgeY))
+    slika_robov = edges_mag.astype(np.uint8)
+
+    return slika_robov
+
+img = cv2.imread("images/Slika_1.jpg", cv2.IMREAD_GRAYSCALE)
+
+img_roberts = my_roberts(img)
+
+cv2.imwrite("images/roberts.jpg", img_roberts)
