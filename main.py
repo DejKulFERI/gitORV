78dfb43e80bc3a93acee1acf1359af99fbcab2c1
DejKulFERI <dejan.kulovec@student.um.si>
2023-04-02 21:29:52 +0200

    Cetrti Commit

--- main.py
+++ main.py
@@ -15,8 +15,25 @@ def my_roberts(slika):
 
     return slika_robov
 
+def my_prewitt(slika):
+    x = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    y = np.array([[-1, -1, -1], [0, 0, 0], [1, 1, 1]])
+
+    slika = slika.astype(np.float32)
+
+    edgeX = cv2.filter2D(slika, -1, x)
+    edgeY = cv2.filter2D(slika, -1, y)
+
+    edges_mag = np.sqrt(np.square(edgeX) + np.square(edgeY))
+
+    slika_robov = edges_mag.astype(np.uint8)
+
+    return slika_robov
+
 img = cv2.imread("images/Slika_1.jpg", cv2.IMREAD_GRAYSCALE)
 
 img_roberts = my_roberts(img)
+img_prewitt = my_prewitt(img)
 
 cv2.imwrite("images/roberts.jpg", img_roberts)
+cv2.imwrite("images/prewitt.jpg", img_prewitt)
